026f4bae3ad80a836885a9071224e674a43c0958
ezann <rayzanmuhammadr774@gmail.com>
2025-08-13 21:48:09 +0700

    add: game variable

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ let createRect = (x, y, width, height, color) => {
   canvasContex.fillRect(x, y, width, height);
 };
 
+const DIRECTION_RIGHT = 4;
+const DIRECTION_UP = 3;
+const DIRECTION_LEFT = 2;
+const DIRECTION_BOTTOM = 1;
+let lives = 3;
+let ghostCount = 4;
+let ghostImageLocations = [
+    { x: 0, y: 0 },
+    { x: 176, y: 0 },
+    { x: 0, y: 121 },
+    { x: 176, y: 121 },
+];
+
+// Game variables
+let fps = 30;
+let pacman;
+let oneBlockSize = 20;
+let score = 0;
+let ghosts = [];
+let wallSpaceWidth = oneBlockSize / 1.6;
+let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
+let wallInnerColor = "black";
